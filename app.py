63fcafd457275c6badec5c4368b5fca40216168c
zepgar93 <zepgar93@gmail.com>
2025-11-01 00:34:01 -0600

    nuevos archivos

---
+++ app.py
@@ -0,0 +1,11 @@
+# Importa la librería Streamlit
+import streamlit as st
+
+# Título de la aplicación web
+st.title('Análisis Exploratorio de Datos de Vehículos')
+
+# Un encabezado simple
+st.header('Configuración Exitosa!')
+
+# Un texto para confirmar que todo está funcionando
+st.write('¡Streamlit está activo! Ahora podemos comenzar con el análisis de datos (EDA).')
